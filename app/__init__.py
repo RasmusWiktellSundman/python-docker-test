ee363b5dca1923c4f8bdbb21f993f6da5fae72ed
RasmusWiktellSundman <49868943+RasmusWiktellSundman@users.noreply.github.com>
2022-04-18 09:43:18 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,3 @@ from .common import auth_utils
 
 from .resources import test
 api.add_resource(test.Test, '/test/<message>')
-api.add_resource(test.Test, '/bingoo/is/<message>')
-api.add_resource(test.Test, '/thenerdyhamster/is/<message>')
